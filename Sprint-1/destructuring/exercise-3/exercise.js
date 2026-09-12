f99d95b93f7c0f63af967943a459cbae6a5d8966
mandip sanger <mandipsanger1@gmail.com>
2026-09-12 14:27:39 +0100

    feature/destructuring3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,16 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+let totalCost = 0;
+
+for (const { itemName, quantity, unitPricePence } of order) {
+  const itemTotal = quantity * unitPricePence;
+
+  console.log(
+    `${quantity}       ${itemName.padEnd(20)}${(itemTotal / 100).toFixed(2)}`
+  );
+
+  totalCost += itemTotal;
+}
+
+console.log(`Total: ${(totalCost / 100).toFixed(2)}`);
